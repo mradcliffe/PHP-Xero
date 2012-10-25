5663140d33171de06f1e06e607745cb094aec80d
Matthew Radcliffe <mradcliffe@softpixel.com>
2012-10-25 17:42:53 -0400

    Fix API breaking change that causes undefined warnings in $arguments and $where.

--- xero.php
+++ xero.php
@@ -108,13 +108,14 @@ class Xero {
 			throw new XeroException('The selected method does not exist. Please use one of the following methods: '.implode(', ',$methods_map));
 		}
 		if ( (count($arguments) == 0) || ( is_string($arguments[0]) ) || ( is_numeric($arguments[0]) ) || ( $arguments[0] === false ) ) {
+			$where = false;
 			//it's a GET request
 			if ( !in_array($name, $valid_get_methods) ) {
 				return false;
 			}
 			$filterid = ( count($arguments) > 0 ) ? strip_tags(strval($arguments[0])) : false;
-			if($arguments[1]!=false) $modified_after = ( count($arguments) > 1 ) ? str_replace( 'X','T', date( 'Y-m-dXH:i:s', strtotime($arguments[1])) ) : false;
-			if($arguments[2]!=false) $where = ( count($arguments) > 2 ) ? $arguments[2] : false;
+			if(isset($arguments[1])) $modified_after = ( count($arguments) > 1 ) ? str_replace( 'X','T', date( 'Y-m-dXH:i:s', strtotime($arguments[1])) ) : false;
+			if(isset($arguments[2])) $where = ( count($arguments) > 2 ) ? $arguments[2] : false;
 			if ( is_array($where) && (count($where) > 0) ) {
 				$temp_where = '';
 				foreach ( $where as $wf => $wv ) {
